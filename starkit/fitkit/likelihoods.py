742152b1b35db4dbc5632eb22038dd73f1d9e5a8
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2015-05-11 18:56:41 +0200

    added some likelihood thing and changed rtd requirements again

--- starkit/fitkit/likelihoods.py
+++ starkit/fitkit/likelihoods.py
@@ -1,5 +1,19 @@
 from astropy import modeling
+import numpy as np
 
 class Chi2Likelihood(modeling.Model):
     inputs = ('wavelength', 'flux')
     outputs = ('loglikelihood')
+
+    def __init__(self, observed):
+        self.observed_wavelength = observed.wavelength.to(u.angstrom).value
+        self.observed_flux = observed.flux.value
+        self.observed_uncertainty = getattr(observed.uncertainty, None)
+        if self.observed_uncertainty is not None:
+            self.observed_uncertainty = self.observed_uncertainty.value
+
+
+    def evaluate(self, wavelength, flux):
+        return -0.5 * np.sum(
+            ((self.observed_flux - flux) / self.observed_uncertainty)**2)
+
